98899c1988d979211bed3efdb6e4fb7b59aa109b
julian villadae <julimauvi@gmail.com>
2021-03-18 10:41:37 +0100

    primer commit

---
+++ App.js
@@ -0,0 +1,137 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div classNameName='grid-container'>
+    <header className="row">
+        <div>
+        <a className="brand" href='index.html'>Amazona</a>
+        </div>
+        <div>
+        <a href='cart.html'>Cart</a>
+        <a href='signin.html'>Sig In</a>
+        </div>
+    </header>
+
+    <main>
+      <div classNameName="row center">
+        <div className="card">
+            <a href="product.html">
+            {/* <!-- image sze 680px by 830px --> */}
+            <img className="medium" src="./images/home.jpg" alt="product" /></a>
+          <div className="card-body">
+            <a href="product.html">
+            <h2>Home</h2>
+            </a>
+           <div className="rating">
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star-half-o "></i></span>  
+           </div>
+           <div className="price">$120</div>
+          </div>
+        </div>
+        <div className="card">
+            <a href="product.html">
+            {/* <!-- image sze 680px by 830px --> */}
+            <img className="medium" src="./images/home.jpg" alt="product" /></a>
+          <div className="card-body">
+            <a href="product.html">
+            <h2>Home</h2>
+            </a>
+           <div className="rating">
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>  
+           </div>
+           <div className="price">$120</div>
+          </div>
+        </div>
+        <div className="card">
+            <a href="product.html">
+            {/* <!-- image sze 680px by 830px --> */}
+            <img className="medium" src="./images/home.jpg" alt="product" /></a>
+          <div className="card-body">
+            <a href="product.html">
+            <h2>Home</h2>
+            </a>
+           <div className="rating">
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>  
+           </div>
+           <div className="price">$120</div>
+          </div>
+        </div>
+        <div className="card">
+            <a href="product.html">
+            {/* <!-- image sze 680px by 830px --> */}
+            <img className="medium" src="./images/home.jpg" alt="product" /></a>
+          <div className="card-body">
+            <a href="product.html">
+            <h2>Home</h2>
+            </a>
+           <div className="rating">
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>  
+           </div>
+           <div className="price">$120</div>
+          </div>
+        </div>
+        <div className="card">
+            <a href="product.html">
+            {/* <!-- image sze 680px by 830px --> */}
+            <img className="medium" src="./images/home.jpg" alt="product" /></a>
+          <div className="card-body">
+            <a href="product.html">
+            <h2>Home</h2>
+            </a>
+           <div className="rating">
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>  
+           </div>
+           <div className="price">$120</div>
+          </div>
+        </div>
+        <div className="card">
+            <a href="product.html">
+            {/* <!-- image sze 680px by 830px --> */}
+            <img className="medium" src="./images/home.jpg" alt="product" /></a>
+          <div className="card-body">
+            <a href="product.html">
+            <h2>Home</h2>
+            </a>
+           <div className="rating">
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>
+              <span>  <i className="fa fa-star"></i></span>  
+           </div>
+           <div className="price">$120</div>
+          </div>
+        </div>
+      </div>
+      </main>
+            <footer className="row center">
+                All right reserved
+            </footer>
+        </div>
+
+
+  );
+}
+
+export default App;
